8539a6f2d6915cbefc890de7f6a1f547f3334502
nhungoc20301 <80160803+nhungoc20301@users.noreply.github.com>
2021-07-23 19:35:59 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+(function($) {
+
+	"use strict";
+
+	$('nav .dropdown').hover(function(){
+		var $this = $(this);
+		$this.addClass('show');
+		$this.find('> a').attr('aria-expanded', true);
+		$this.find('.dropdown-menu').addClass('show');
+	}, function(){
+		var $this = $(this);
+			$this.removeClass('show');
+			$this.find('> a').attr('aria-expanded', false);
+			$this.find('.dropdown-menu').removeClass('show');
+	});
+
+})(jQuery);
+$(document).ready( function() {   
+
+$('.grid').isotope({
+  itemSelector: '.grid-item',
+});
+
+// filter items on button click
+$('.filter-button-group').on( 'click', 'li', function() {
+  var filterValue = $(this).attr('data-filter');
+  $('.grid').isotope({ filter: filterValue });
+  $('.filter-button-group li').removeClass('active');
+  $(this).addClass('active');
+});
+    })
